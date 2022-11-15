392adfbf985c8e1dcaa9dd27f925ea76a86f8ee8
mortal Wombat <83345498+ThatOneJon@users.noreply.github.com>
2022-11-15 13:44:02 +0100

    cors setup

---
+++ api/permissions.py
@@ -0,0 +1,4 @@
+from rest_framework import permissions
+
+
+# this only works with generic views ---_> NOT functional views, but class based!!
--- fullStackBack/settings.py
+++ fullStackBack/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'fiTra', 'rest_framework'
+    'fiTra', 'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'fullStackBack.urls'
@@ -166,3 +169,5 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
+
+CORS_ALLOW_ALL_ORIGINS: True
